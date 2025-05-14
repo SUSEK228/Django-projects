e6ed62c1530a34b13f943e0ba93e1d39b2209925
SUSEK228 <148262852+SUSEK228@users.noreply.github.com>
2025-05-14 17:00:07 +0200

    dodano todo liste i logout

--- My_web/web_app/urls.py
+++ My_web/web_app/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path('', views.main, name='main'),
     path('login', views.login, name='login'),
     path('register', views.register, name='register'),
+    path('logout', views.logout, name='logout' ),
+    path('todo', views.todo, name='todo'),
+    path('update_task/<str:pk>/', views.update_task, name = 'update_task'),
+    path('delete_task/<str:pk>/', views.delete_task, name="delete_task"),
     
 ]
--- My_web/web_app/views.py
+++ My_web/web_app/views.py
@@ -1,11 +1,80 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from . models import Task
+from . forms import TaskForm
 
 # Create your views here.
 def main(request):
     return render(request, 'main.html')
 
 def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = auth.authenticate(username=username,password=password)
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/')
+        else:
+            messages.info(request, ' User does not exist')
+            return redirect('login')
     return render(request, 'login.html')
 
 def register(request):
-    return render(request, 'register.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'This email is already use')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info('This username is already use')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username,email=email,password=password)
+                user.save()
+                return redirect('login')
+        messages.info(request, 'Your passwords are not the same')
+        return redirect('register')
+    else:
+        return render(request, 'register.html')
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+def todo(request):
+    form = TaskForm()
+    tasks = Task.objects.all()
+    if request.method=='POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('todo')
+    
+    context = {'tasks': tasks, 'TaskForm' : form}
+    return render(request, 'todo.html', context)
+
+def update_task(request, pk):
+    task = Task.objects.get(id=pk)
+    form = TaskForm(instance=task)
+    
+    if request.method=='POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('todo')
+    context = {'TaskForm' : form}
+    return render(request, 'update_task.html', context)
+
+def delete_task(request, pk):
+    task= Task.objects.get(id=pk)
+    
+    if request.method == 'POST':
+        task.delete()
+        return redirect('todo')
+    context = {'task': task}
+    return render(request, 'delete_task.html', context)
